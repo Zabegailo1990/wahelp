ac13ac06ee679a65d315149e9a4e03e9f7d8f578
Zabegailo1990 <zvv56739@gmail.com>
2023-11-29 01:26:47 +0300

    ++

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://github.com/Zabegailo1990/wahelp.git',
+  base: '/wahelp/',
 })
